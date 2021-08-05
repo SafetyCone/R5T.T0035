9e2eae099948fb27964f6167e63bfa5b1341e900
David Coats <david@getsafetycone.com>
2021-08-05 13:15:28 -0700

    Initial commit of source-stub. Add INamespaceName extension method base.

---
+++ source/R5T.T0035/Code/Documentation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.T0035
+{
+    /// <summary>
+    /// <see cref="INamespaceName"/> extension method base types.
+    /// </summary>
+    public class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0035/Code/Interfaces/INamespaceName.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0035
+{
+    /// <summary>
+    /// Empty interface as base for extension methods.
+    /// </summary>
+    public interface INamespaceName
+    {
+    }
+}

9e36ef36bfd9e976a5a32abcdd8bf662f7602214
David Coats <david@getsafetycone.com>
2022-01-24 17:03:00 -0800

    Move INamespaceNameExtensions to canonical location. Add functionality to set usings using a UsingDirectivesSpecification instance.

---
+++ source/R5T.T0035.T002/Code/Extensions/CompilationUnitSyntaxExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using R5T.L0011.T004;
+
+
+namespace System
+{
+    public static class CompilationUnitSyntaxExtensions
+    {
+        /// <summary>
+        /// Handles both using namespace name directives and name aliases.
+        /// </summary>
+        public static CompilationUnitSyntax SetUsings(this CompilationUnitSyntax compilationUnit,
+            UsingDirectivesSpecification usingDirectivesSpecification)
+        {
+            var namespaceNames = usingDirectivesSpecification.GetNamespaceNameSet();
+
+            var usingDirectiveBlocks = namespaceNames.GetBlocks();
+
+            var nameAliases = usingDirectivesSpecification.NameAliases
+                .Select(x => x.GetUsingDirectiveSyntax().AppendBlankLine())
+                .Now();
+
+            var output = compilationUnit
+                .SetUsings(usingDirectiveBlocks)
+                .AddNameAliases(nameAliases);
+
+            return output;
+        }
+    }
+}
